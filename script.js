bfa42c5f9c501c724b75a0462f903a4f9ded00c7
Lucas Tenani <lucas.tenani3@gmail.com>
2022-01-06 18:15:13 -0300

    Now I created a script so that Navbar can be closed too

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const bar = document.getElementById("bar");
+const closeNav = document.getElementById("close");
 const nav = document.getElementById("navbar");
 
 if (bar) {
   bar.addEventListener("click", () => {
     nav.classList.add("active");
+  });
+}
+
+if (closeNav) {
+  closeNav.addEventListener("click", () => {
+    nav.classList.remove("active");
   });
 }
