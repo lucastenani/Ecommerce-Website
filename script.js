7f13e0cae72013b371ea6aa1ea281a89e90821fc
Lucas Tenani <lucas.tenani3@gmail.com>
2022-01-08 19:44:02 -0300

    A script was created so when you click on the small image of the product, the product goes to the main image

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ if (closeNav) {
     nav.classList.remove("active");
   });
 }
+
+var MainImg = document.getElementById("MainImg");
+var smallImg = document.getElementsByClassName("small-img");
+if (MainImg) {
+  smallImg[0].onclick = function () {
+    console.log(smallImg[0].src);
+    MainImg.src = smallImg[0].src;
+  };
+
+  smallImg[1].onclick = function () {
+    MainImg.src = smallImg[1].src;
+  };
+
+  smallImg[2].onclick = function () {
+    MainImg.src = smallImg[2].src;
+  };
+
+  smallImg[3].onclick = function () {
+    MainImg.src = smallImg[3].src;
+  };
+}
